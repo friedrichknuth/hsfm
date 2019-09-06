3c57737e8acafdb765a2686c7f8579e2cb4e887e
Friedrich Knuth <knuth@uw.edu>
2019-09-06 10:28:58 -0700

    improve io.split_file()

--- hsfm/io/io.py
+++ hsfm/io/io.py
@@ -12,8 +12,8 @@ def create_dir(directory):
 
 def split_file(file_path_and_name):
     file_path = os.path.split(file_path_and_name)[0]
-    file_name = os.path.split(file_path_and_name)[-1].split('.')[0]
-    file_extension = '.'+os.path.split(file_path_and_name)[-1].split('.')[-1]
+    file_name = os.path.splitext(os.path.split(file_path_and_name)[-1])[0]
+    file_extension = os.path.splitext(os.path.split(file_path_and_name)[-1])[-1]
     
     return file_path, file_name, file_extension
     
