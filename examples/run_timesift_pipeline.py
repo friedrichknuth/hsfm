bc849024496c94406ea096aa5465b50cce0c1bd1
elischwat <eli.l.schwat@gmail.com>
2021-07-21 00:07:01 -0700

    clean up and update example scripts for running pipelines

--- examples/run_timesift_pipeline.py
+++ examples/run_timesift_pipeline.py
@@ -1,42 +1,28 @@
 from hsfm.pipeline import TimesiftPipeline
-# from hsfm.pipeline import NAGAPPreprocessingPipeline
+from hsfm.pipeline import NAGAPPreprocessingPipeline
 
-# preprocess_pipeline = NAGAPPreprocessingPipeline(
-#     output_directory,
-#     fiducial_templates_directory = "/data2/elilouis/generate_ee_dems_baker/fiducials",
-#     nagap_images_csv_path,
-#     bounds
-# )
+# Example script running preprocessing and and timesift processing for all NAGAP images within bounds near Mt Baker.
 
-# preprocess_pipeline.run()
+preprocess_pipeline = NAGAPPreprocessingPipeline(
+    "/data2/elilouis/timesift/baker",
+    fiducial_templates_directory = "/home/elilouis/hipp/examples/fiducial_proxy_detection/input_data/fiducials/nagap/",
+    nagap_images_csv_path = "/home/elilouis/hipp/examples/fiducial_proxy_detection/input_data/nagap_image_metadata_updated_manual.csv",
+    bounds = "-121.94 48.84 -121.70 48.70"
+)
 
-# timesift_pipeline = TimesiftPipeline(
-#     metashape_metadata_file = "/data2/elilouis/generate_ee_dems_baker/mixed_timesift/combined_metashape_metadata.csv",
-#     image_metadata_file = "/data2/elilouis/generate_ee_dems_baker/mixed_timesift/combined_image_metadata.csv",
-#     raw_images_directory = "/data2/elilouis/generate_ee_dems_baker/mixed_timesift/preprocessed_images/",
-#     output_directory = "/data2/elilouis/generate_ee_dems_baker/mixed_timesift/",
-#     reference_dem_lowres = '/data2/elilouis/hsfm-geomorph/data/reference_dem_highres/baker/baker_2015_utm_10m.tif',
-#     reference_dem_hires = '/data2/elilouis/hsfm-geomorph/data/reference_dem_highres/baker/baker_2015_utm_m.tif',
-#     image_matching_accuracy = 2,
-#     densecloud_quality = 4,
-#     output_DEM_resolution = 5,
-#     license_path="/home/elilouis/hsfm/uw_agisoft.lic",
-#     parallelization=1
-# )
-# timesift_pipeline.run()
+preprocess_pipeline.run()
 
-# These two csv files just have a small subset of the images
 timesift_pipeline = TimesiftPipeline(
-    metashape_metadata_file = "/data2/elilouis/generate_ee_dems_baker/mixed_timesift_test/combined_metashape_metadata.csv",
-    image_metadata_file = "/data2/elilouis/generate_ee_dems_baker/mixed_timesift_test/combined_image_metadata.csv",
-    raw_images_directory = "/data2/elilouis/generate_ee_dems_baker/mixed_timesift/preprocessed_images/",
-    output_directory = "/data2/elilouis/generate_ee_dems_baker/mixed_timesift_test/",
-    reference_dem_lowres = '/data2/elilouis/hsfm-geomorph/data/reference_dem_highres/baker/baker_2015_utm_10m.tif',
-    reference_dem_hires = '/data2/elilouis/hsfm-geomorph/data/reference_dem_highres/baker/baker_2015_utm_m.tif',
-    image_matching_accuracy = 5,
-    densecloud_quality = 5,
-    output_DEM_resolution = 5,
-    license_path="/home/elilouis/hsfm/uw_agisoft.lic",
-    parallelization=1
+    metashape_metadata_file =   "/data2/elilouis/timesift/baker/metashape_metadata.csv",
+    image_metadata_file =       "/data2/elilouis/timesift/baker/image_metadata.csv",
+    raw_images_directory =      "/data2/elilouis/timesift/baker/preprocessed_images/",
+    output_directory =          "/data2/elilouis/timesift/baker/",
+    reference_dem_lowres =      "/data2/elilouis/hsfm-geomorph/data/reference_dem_highres/baker/baker_2015_utm_10m.tif",
+    reference_dem_hires =       "/data2/elilouis/hsfm-geomorph/data/reference_dem_highres/baker/baker_2015_utm_m.tif",
+    image_matching_accuracy =   1,
+    densecloud_quality =        2,
+    output_DEM_resolution =     1,
+    license_path =              "/home/elilouis/hsfm/uw_agisoft.lic",
+    parallelization =           1
 )
 timesift_pipeline.run()
--- examples/run_timesift_pipeline.sh
+++ examples/run_timesift_pipeline.sh
@@ -4,7 +4,7 @@
 ###########################################################################################
 # FIRST STEP
 python hsfm/timesift/preprocessing.py \
-    --output-directory "/data2/elilouis/mt_baker_timesift_cam_calib/" \
+    --output-directory "/data2/elilouis/timesift/baker" \
     --fiducial-templates-directory "/home/elilouis/hipp/examples/fiducial_proxy_detection/input_data/fiducials/nagap/" \
     --nagap-images-csv-path "/home/elilouis/hipp/examples/fiducial_proxy_detection/input_data/nagap_image_metadata_updated_manual.csv" \
     --bounds "-121.94 48.84 -121.70 48.70"
@@ -20,10 +20,10 @@ python hsfm/timesift/preprocessing.py \
 # an output resolution, a Metashape license path, and parallelization value.
 
 python hsfm/timesift/timesift.py \
-    --metashape-metadata-file   "/data2/elilouis/mt_baker_timesift_cam_calib/metashape_metadata.csv" \
-    --image-metadata-file       "/data2/elilouis/mt_baker_timesift_cam_calib/image_metadata.csv" \
-    --raw-images-directory      "/data2/elilouis/mt_baker_timesift_cam_calib/preprocessed_images/" \
-    --output-directory          "/data2/elilouis/mt_baker_timesift_cam_calib/" \
+    --metashape-metadata-file   "/data2/elilouis/timesift/baker/metashape_metadata.csv" \
+    --image-metadata-file       "/data2/elilouis/timesift/baker/image_metadata.csv" \
+    --raw-images-directory      "/data2/elilouis/timesift/baker/preprocessed_images/" \
+    --output-directory          "/data2/elilouis/timesift/baker/" \
     --reference-dem-lowres      "/data2/elilouis/hsfm-geomorph/data/reference_dem_highres/baker/baker_2015_utm_10m.tif" \
     --reference-dem-hires       "/data2/elilouis/hsfm-geomorph/data/reference_dem_highres/baker/baker_2015_utm_m.tif" \
     --densecloud-quality        2 \
